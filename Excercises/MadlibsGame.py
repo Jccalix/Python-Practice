bbb0bab3d5bef47f927c32481d3bfb9b650e4cb7
Jccalix <jccalix6@gmail.com>
2025-08-11 00:06:46 -0600

    Add MadlibsGame.py for interactive storytelling experience

---
+++ Excercises/MadlibsGame.py
@@ -0,0 +1,11 @@
+print("Welcome to the Mad Libs Game!")
+print("Please provide the following information:")
+
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+
+mad_lib = f"Once upon a time, {name} went to {place} to {verb}. It was a {adjective} day."
+print(mad_lib)
+print("Thank you for playing the Mad Libs Game!")
