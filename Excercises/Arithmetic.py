c8ac1c6085df39effcc6c55b2414e72e3cb85a19
Jccalix <jccalix6@gmail.com>
2025-08-11 00:19:29 -0600

    Add exercises for calculating area, circumference, and properties of circles

---
+++ Excercises/Arithmetic.py
@@ -0,0 +1,12 @@
+x = 5
+y = 10
+z = x + y
+
+result = pow(z, 2)
+print("The sum of", x, "and", y, "is", z)
+print("The square of the sum is", result)
+
+max_value = max(x, y, z)
+min_value = min(x, y, z)      
+print("The maximum value is", max_value)
+print("The minimum value is", min_value)
---
+++ Excercises/Pi.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.pi)
+x = 8.9
+
+result = math.sqrt(x)
+print("The square root of", x, "is", result)
+result = math.ceil(x)
+print("The ceiling of", x, "is", result)
+result = math.floor(x)
+print("The floor of", x, "is", result)
