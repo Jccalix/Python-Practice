756da09887c7059a09daf238aedc35d35a1f8e32
Jccalix <jccalix6@gmail.com>
2025-08-13 00:53:06 -0600

    Add exercises for calculating the hypotenuse and evaluating age categories

---
+++ Excercises/HypotenuseTriangle.py
@@ -0,0 +1,8 @@
+import math
+
+a=input("Enter the length of side a: ")
+b=input("Enter the length of side b: ")
+
+c=math.sqrt(float(a)**2 + float(b)**2)
+
+print("The length of the hypotenuse is:", c)
